9860e606f17c79c675bf40b58a8038bf5acfaec0
AmirAfz-Dev <59703432+AmirAfz-Dev@users.noreply.github.com>
2020-05-19 00:42:33 +0700

    Add files via upload

---
+++ Countingsundays.py
@@ -0,0 +1,12 @@
+#Amir Afzali
+
+import datetime
+#The best way to approach the problem would be to take advantage of the datetime library as it should have all the info I need so import it
+
+#Setting up the ranges for month and years
+for year in range(1901,2001):
+    for month in range(1,13):
+        if datetime()
+
+        
+
